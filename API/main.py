7fc96f4d0c1ebdae43e0b414b1bfb563f8d71e69
tiagohorta21 <tiago_horta95@hotmail.com>
2021-05-31 13:41:02 +0100

    SM-7: Add sensors form

--- API/main.py
+++ API/main.py
@@ -1,5 +1,4 @@
-from flask import Flask, jsonify, render_template
-from flask import request
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
@@ -14,25 +13,28 @@ def homeScreen():
 
 @app.route('/sensors/create-sensor', methods=['GET', 'POST'])
 def createSensor():
-    # Open database connection
-    connection = sqlite3.connect("TP2.db")
-    cursor = connection.cursor()
+    if request.method == 'POST' and request.form['submit_button'] == 'Submit':
+        # Open database connection
+        connection = sqlite3.connect("TP2.db")
+        cursor = connection.cursor()
 
-    # Access received data from the url
-    name = request.args.get('name')
-    unit = request.args.get('unit')
+        # Access received data from the url
+        name = request.form.get('name')
+        unit = request.form.get('unit')
 
-    # Adds a new sensor
-    cursor.execute('INSERT INTO Sensor (idSensor, idLocation, name, unit) VALUES (?,?,?,?)',
-                   (None, None, name, unit))
+        # Adds a new sensor
+        cursor.execute('INSERT INTO Sensor (idSensor, idLocation, name, unit) VALUES (?,?,?,?)',
+                       (None, None, name, unit))
 
-    # Save (commit) the changes
-    connection.commit()
-    # We can also close the connection if we are done with it
-    # Just be sure any changes have been committed or they will be lost.
-    connection.close()
+        # Save (commit) the changes
+        connection.commit()
+        # We can also close the connection if we are done with it
+        # Just be sure any changes have been committed or they will be lost.
+        connection.close()
 
-    return 'Sensor created successfully'
+        return 'Sensor created successfully'
+    elif request.method == 'GET':
+        return render_template('sensor.html')
 
 
 @app.route('/sensors', methods=['GET'])
