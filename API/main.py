28171d9850af7be2bfd8279f5fef39bfce537da3
dariocruz96 <dariocruz96@hotmail.com>
2021-05-31 19:48:47 +0100

    SM-5: Delete a sensor

--- API/main.py
+++ API/main.py
@@ -1,3 +1,5 @@
+from django import db
+from django.shortcuts import redirect
 from flask import Flask, jsonify, render_template
 from flask import request
 import sqlite3
@@ -52,6 +54,25 @@ def listSensor():
     connection.close()
 
     return sensorsList
+
+
+@app.route('/delete/<id>')
+def deleteSensor(id):
+    # Open database connection
+    connection = sqlite3.connect("TP2.db")
+    cursor = connection.cursor()
+
+    cursor.execute("PRAGMA FOREIGN_KEYS = ON")
+    cursor.execute('DELETE FROM Sensor WHERE idSensor= ?', id)
+    # friend_to_delete = Sensor.query.get_or_404(id)
+
+    # Save (commit) the changes
+    connection.commit()
+    # We can also close the connection if we are done with it
+    # Just be sure any changes have been committed or they will be lost.
+    connection.close()
+
+    return 'Sensor deleted successfully'
 
 
 if __name__ == '__main__':
